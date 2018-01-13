4e46c64a72c374a646d1ef8fc6a1675868b795a8
wangpeng <wangpeng@beescm.com>
2018-01-13 14:49:19 +0800

    Initial commit

---
+++ src/mn.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const { mn } = require('./config/default');
+const srcToImg = require('./helper/srcToImg');
+
+(async _ => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('http://image.baidu.com/')
+    console.log('go to http://image.baidu.com/')
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    })
+    console.log('reset viewport')
+    await page.focus('#kw')
+    await page.keyboard.sendCharacter('狗')
+    await page.click('.s_search')
+    console.log('go to search list')
+    page.on('load', async _ => {
+        console.log('page loading done, start fetch ...')
+        const srcs = await page.evaluate(_ => {
+            const images = document.querySelectorAll('img.main_img')
+            return Array.prototype.map.call(images, img => img.src)
+        })
+        console.log(`get ${srcs.length} images, start download`)
+        srcs.forEach(async src => {
+            // sleep 反 反爬虫
+            await page.waitFor(200)
+            await srcToImg(src, mn)
+        })
+        await browser.close()
+    })
+})()
